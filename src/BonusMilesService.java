c82129f8eb3f90bf2535eecb98af14c1f56593df
AnastasiaIQA12 <Sinka8900@mail.ru>
2020-11-04 12:34:47 +0300

    Ghange project

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int summa_one_mile = 20;
-        int amount_mile = (int) (cost / summa_one_mile);
-        return amount_mile;
+        int SummaOneMile = 20;
+        int AmountMile = cost / SummaOneMile;
+        return AmountMile;
     }
 }
